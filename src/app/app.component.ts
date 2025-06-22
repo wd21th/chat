675580a60eeaff938d0f2a77b31e402000e14a7e
Dias <wprogrammist@gmail.com>
2025-06-22 20:14:10 +0500

    add autofocus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   messages: Array<string> = [];
+  @ViewChild('inputEl', { static: true })
+  inputElement?: ElementRef<HTMLInputElement>;
+
+  ngOnInit(): void {
+    this.inputElement?.nativeElement.focus();
+  }
 
   keydown(keyboardEvent: KeyboardEvent) {
     if (keyboardEvent.code === 'Enter') {
